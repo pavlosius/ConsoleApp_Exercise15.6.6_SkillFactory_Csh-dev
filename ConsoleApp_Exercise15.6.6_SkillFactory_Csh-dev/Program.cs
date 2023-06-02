19cd791ffb41861104b85411403caa673f21a1fa
pavlosius <pavlosius@mail.ru>
2023-06-02 17:21:09 +0300

    Add project files.

---
+++ ConsoleApp_Exercise15.6.6_SkillFactory_Csh-dev/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+
+            Console.WriteLine("Программа завершена. Нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            string[] result = classes.SelectMany(c => c.Students).ToArray();
+            return result;
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
